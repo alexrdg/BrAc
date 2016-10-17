d08282d531b695e19cc232de1ee452cd9d9b0cc8
alexrdg <alexandre.rodriguez@eemi.com>
2016-10-17 17:18:28 +0200

    Now can get the data before the content

--- BrAc.php
+++ BrAc.php
@@ -10,19 +10,35 @@ License:
 */
 require_once 'Team.php';
 require_once 'FootballData.php';
+?>
 
-$api = new FootballData();
-// fetch and dump summary data for premier league' season 2015/16
-$soccerseason = $api->getSoccerseasonById(398);
+<?php
 
-foreach ($soccerseason->getFixturesByMatchday(1) as $fixture) { ?>
-    <tr>
-        <td><?php echo $fixture->homeTeamName; ?></td>
-        <td>-</td>
-        <td><?php echo $fixture->awayTeamName; ?></td>
-        <td><?php echo $fixture->result->goalsHomeTeam; ?></td>
-        <td>:</td>
-        <td><?php echo $fixture->result->goalsAwayTeam; ?></td>
-    </tr>
-<?php }
 
+add_filter('wp_head','add_view');
+function add_view() {
+    $api = new FootballData();
+    // fetch and dump summary data for premier league' season 2015/16
+    $soccerseason = $api->getSoccerseasonById(398);
+    ?>
+    <link rel="stylesheet" href="style/style.css">
+    <div class="container">
+        <table style="margin-top: 20px;">
+            <tr>
+                <td style="text-align: center;">A DOMICILE</td>
+                <td style="text-align: center;">A L'EXTERIEUR</td>
+            </tr>
+            <?php foreach ($soccerseason->getFixturesByMatchday(1) as $fixture) { ?>
+                <tr>
+                    <td style="text-align: center;"><?php echo $fixture->homeTeamName; ?><br>
+                        <?php echo $fixture->result->goalsHomeTeam; ?>
+                    </td>
+                    <td style="text-align: center;"><?php echo $fixture->awayTeamName; ?><br>
+                        <?php echo $fixture->result->goalsAwayTeam; ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+    <?php
+}
